ca860fabc469416a8fc00054b4bff177f75f3327
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-13 10:23:39 +0430

    worked on models

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -9,4 +9,7 @@ class Cart extends Model
 {
     /** @use HasFactory<\Database\Factories\CartFactory> */
     use HasFactory;
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
