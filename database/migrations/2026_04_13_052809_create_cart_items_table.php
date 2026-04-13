27262ee8896cc1ad3e3bc48f7f8cf587f30b3088
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-13 10:01:09 +0430

    files added

--- database/migrations/2026_04_13_052809_create_cart_items_table.php
+++ database/migrations/2026_04_13_052809_create_cart_items_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->decimal("price",12,2);
+            $table->integer("qty");
+            $table->foreignId("product_id")->constrained("products")->onDelete("cascade");
             $table->timestamps();
         });
     }
