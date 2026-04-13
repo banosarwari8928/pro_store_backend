7ee06bf88f56e85672a5ce683d434d782a1f80d8
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-13 10:21:31 +0430

    worked on models

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,4 +9,11 @@ class Review extends Model
 {
     /** @use HasFactory<\Database\Factories\ReviewFactory> */
     use HasFactory;
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+     public function user(){
+        return $this->belongsTo(User::class);
+    }
+
 }
