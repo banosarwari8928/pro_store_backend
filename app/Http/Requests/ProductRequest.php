5cc8729d8f18fcb574f67af68d3a7b9bdc4bf4b8
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-16 15:09:36 +0430

    some changes added

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -26,11 +26,11 @@ class ProductRequest extends FormRequest
         return [
             //
             'name'=> ['required' ,'string','min:3', Rule::unique('products','name')],
-            'price'=> 'required|numaric|min:10|max:19000',
-            'stock'=> 'required|integer|min:1|max:200',
-            'brand'=> 'required|string',
+            'price'=> 'required|numaric|min:10|max:19000|nullable',
+            'stock'=> 'required|integer|min:1|max:200'|null,
+            'brand'=> 'required|string'|null,
             'description'=> 'required|string|min:10',
-            'category'=> 'required|string|min:10',
+            'category'=> 'required|strin                                                                                                                                                                                                                                                                                  g|min:10',
         ];
     }
 }
