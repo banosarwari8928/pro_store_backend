ffb8cad450acb4af883af1872c34022f996065b6
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-18 15:20:39 +0430

    worked on update function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -32,33 +32,34 @@ class ProductController extends Controller
      */
     public function store(ProductRequest $request)
     {
-        //
-        $product = new Product();
+           $product = new Product();
         $product->create([
-            'name'=> $request->name,
-            'stock'=> $request->stock,
-            'price'=> $request->price,
+            "name"=> $request->name,
+            "stock"=> $request->stock,
+            "price"=> $request->price,
         ]);
         $product->save();
 
-         $productDetails = new ProductDetail();
-         $productDetails->create([
-            'brand'=>$request->brand,
-            'description'=>$request->description,
-            'category'=>$request->category,
-            'product_id'=>$product->id,
-         ]);
-         $path = null;
-         if($request->hasFile('image')){
-            $path = $request->file('image')->store('Products_Image', 'public');
-         }
-         $image = new Image();
-         $image->create([
-            'img_url'=>$path,
-            'imageable_id'=> $product->id,
-            'imageable_type'=> Product::class,
-         ]);
-         $image->save();
+        $productDetails = new ProductDetail();
+        $productDetails->create([
+            "brand"=> $request->barnd,
+            "description"=> $request->description,
+            "product_id"=> $request->id,
+            "category"=> $request->cat,
+        ]);
+        $productDetails->save();
+        $path = null;
+        if($request->hasFile("image")){
+            $path = $request->file("image")->store("pro_img","public");
+        }
+        $image = new Image();
+        $image->create([
+            "img_url"=> $path,
+            "imageable_id"=> $product,
+            "imageable_type"=> Product::class
+        ]);
+        $image->save();
+    
     }
 
     /**
@@ -77,49 +78,89 @@ class ProductController extends Controller
      */
     public function update(ProudctUpdateREq $request, string $id)
     {
-        //
-        try{
-            $product=Product::findOrFail($id)->with(['images','pro_details'])->First();
-            $product->update([
-                "name"=>$request->name,
-                "price"=>$request->price,
-                "stock"=>$request->stock,
+       $product = Product::findOrFail($id);
+       $product->update([
+        "name"=> $request->name,
+        "stock"=> $request->stock,
+        "price"=> $request->price,
+       ]);
+         $product->save();
+            $productDetails = ProductDetail::where("product_id" , $id)->first();
+            $productDetails->update([
+                "brand" => $request->brand,
+                "description" => $request->description,
+                "category" => $request->category,
             ]);
-            $product->save();
-            $productDetail=ProductDetail::where('product_id', $product->id)->first();
-            $productDetail->update(
-                [
-                    "description"=>$request->discribtion,
-                    "catagory"=>$request->catagory,
-                    "brand"=>$request->brand,
-                ]
-            );
-            $image=null;
-            $image2=null;
-            if($request->hasFile('image1')){
-                $image=$request->file('image1')->store('product_images','public');
+            $productDetails->save();
+            $path = null;
+            if($request->hasFile('image')){
+                $path = $request->file('img_url')->store('images' , 'public');
             }
-           $images= Image::where('imageable_type',Product::class)->where('imageable_id','product_id')->get();
-          for($i = 0; count($imagew)>0;$i++){
-            if($i ==0){
-
-            }else{
-
+            $path2 = null ;
+            if($request->hasFile('image2')){
+               $path2 =  $request->file('image2')->store('images', 'public');
             }
-          }
-           $images->update([
-            "imageable_id"=>$product->id,
-            "img_url"=>$image
-           ]);
-        }
-        catch(Exception $err){
-            return response()->json(
-                [
-                    "error"=>$err->getMessage(),
-                ],
-            );
-        };
+            $image = Image::where("imageable_id" , $id)->where("imageable_type" , Product::class)->first();
+            for($i = 0; count($image)> 0; $i++){
+                if($i === 0){
+                    $image->update([
+                        'img_url' => $path,
+                    ]);
+                }
+                else{
+                    $image->update([
+                        'img_url'=> $path2,
+                    ]);
+                }
+            }
+            return response()->json([
+                "message" => "Product updated successfully",
+            ]);
     }
+    // {
+    //     //
+    //     try{
+    //         $product=Product::findOrFail($id)->with(['images','pro_details'])->First();
+    //         $product->update([
+    //             "name"=>$request->name,
+    //             "price"=>$request->price,
+    //             "stock"=>$request->stock,
+    //         ]);
+    //         $product->save();
+    //         $productDetail=ProductDetail::where('product_id', $product->id)->first();
+    //         $productDetail->update(
+    //             [
+    //                 "description"=>$request->discribtion,
+    //                 "catagory"=>$request->catagory,
+    //                 "brand"=>$request->brand,
+    //             ]
+    //         );
+    //         $image=null;
+    //         $image2=null;
+    //         if($request->hasFile('image1')){
+    //             $image=$request->file('image1')->store('product_images','public');
+    //         }
+    //        $images= Image::where('imageable_type',Product::class)->where('imageable_id','product_id')->get();
+    //       for($i = 0; count($image)>0;$i++){
+    //         if($i ==0){
+
+    //         }else{
+
+    //         }
+    //       }
+    //        $images->update([
+    //         "imageable_id"=>$product->id,
+    //         "img_url"=>$image
+    //        ]);
+    //     }
+    //     catch(Exception $err){
+    //         return response()->json(
+    //             [
+    //                 "error"=>$err->getMessage(),
+    //             ],
+    //         );
+    //     };
+    // }
 
     /**
      * Remove the specified resource from storage.
--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -13,7 +13,7 @@ class ProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
